7f89907d95c127e7243ddb675d08f32cdea3a595
Opebiyi Oluwafemi <opebiyibiodun10@gmail.com>
2023-10-31 21:42:37 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const qrImg = document.querySelector('img');
+
+const linkInput = document.querySelector('.input');
+
+const generateBtn = document.querySelectorAll('button')
+
+
+// let data =  {
+//     content: 'hello world',
+//     size: '100*100'
+
+// }
+
+generateBtn[0].addEventListener('click',()=>{
+    generateQr()
+})
+
+
+
+
+function generateQr(){
+    let linkValue = linkInput.value
+    fetch(`http://api.qrserver.com/v1/create-qr-code/?data=${linkValue}&size=150x150`,{
+        method: "POST",
+        
+    }).then((res)=>{
+        return res
+    }).then((result)=>{
+        let imageLink = result.url
+        qrImg.src = imageLink
+    })
+}
+
